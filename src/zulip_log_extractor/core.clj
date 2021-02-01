015624b71d13f14855262a930964d7b81195d6a6
Robert Hambrock <roberthambrock@gmail.com>
2021-02-01 01:36:26 +0100

    create basic extractor

--- src/zulip_log_extractor/core.clj
+++ src/zulip_log_extractor/core.clj
@@ -1,7 +1,18 @@
 (ns zulip-log-extractor.core
-  (:use [clojure.java.shell :only [sh]]
-        [clojure.data.json :as json])
+  (:use [clojure.data.json :as json])
   (:gen-class))
 
-(defn -main []
-  (println "Hello world"))
+(def data (slurp "messages-000001.json"))
+
+(def json (second (first (json/read-str data :key-fn keyword))))
+
+(def meetings (filter #(re-find #"meeting*" %) (distinct (map :subject json))))
+
+(defn extract-messages [subject messages]
+  (map :content (filter #(re-matches (re-pattern subject) (:subject %)) messages))
+  )
+
+(defn file-name [meeting-name]
+  (str (clojure.string/replace meeting-name #" " "-") ".log"))
+
+(map #(spit (file-name %) (clojure.string/join "\n---\n" (extract-messages % json))) meetings)
